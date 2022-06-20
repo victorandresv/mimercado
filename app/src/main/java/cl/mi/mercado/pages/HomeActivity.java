17418ce5912413268d09a3fa8737f0e0c3dabbe3
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-20 18:58:27 -0300

    Tabs added

--- app/src/main/java/cl/mi/mercado/pages/HomeActivity.java
+++ app/src/main/java/cl/mi/mercado/pages/HomeActivity.java
@@ -1,12 +1,18 @@
 package cl.mi.mercado.pages;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import cl.mi.mercado.R;
+import cl.mi.mercado.adapters.HomeFragmentStateAdapter;
+import cl.mi.mercado.fragments.ProductsFragment;
+import cl.mi.mercado.fragments.SalesFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,6 +27,26 @@ public class HomeActivity extends AppCompatActivity {
 
         getSupportActionBar().setSubtitle(getResources().getString(R.string.home));
 
-        findViewById(R.id.btnNewSell).setOnClickListener(view -> startActivity(new Intent(context, CartActivity.class)));
+        HomeFragmentStateAdapter hfsa = new HomeFragmentStateAdapter(getSupportFragmentManager(),getLifecycle());
+        hfsa.addFragment(new SalesFragment());
+        hfsa.addFragment(new ProductsFragment());
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager);
+        viewPager2.setAdapter(hfsa);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> {
+                    switch (position){
+                        case 0:
+                            tab.setText(getResources().getText(R.string.sales));
+                            break;
+                        case 1:
+                            tab.setText(getResources().getText(R.string.products));
+                            break;
+                    }
+                }
+        ).attach();
+
+        //findViewById(R.id.btnNewSell).setOnClickListener(view -> startActivity(new Intent(context, CartActivity.class)));
     }
 }
