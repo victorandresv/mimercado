c5d82d05af63dbfb467355ea0eb1d5350a6ed74e
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-20 09:29:37 -0300

    zxing added

--- app/src/main/java/cl/mi/mercado/pages/CartActivity.java
+++ app/src/main/java/cl/mi/mercado/pages/CartActivity.java
@@ -1,12 +1,16 @@
 
 package cl.mi.mercado.pages;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import cl.mi.mercado.R;
 
@@ -27,7 +31,21 @@ public class CartActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         getSupportActionBar().setSubtitle(getResources().getString(R.string.state_account));
+
+        findViewById(R.id.btnScan).setOnClickListener(view -> {
+            barcodeLauncher.launch(new ScanOptions());
+        });
+
     }
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+        result -> {
+            if(result.getContents() == null) {
+                Toast.makeText(context, "Escaneo de codigo de barras cancelado", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     @Override
     public boolean onSupportNavigateUp() {
